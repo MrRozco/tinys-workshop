6b67c447024e7a6f09c825bac9bc5153a054f4de
MrRozco <bryanorozco1997@hotmail.com>
2024-08-13 19:41:48 -0400

    laptop

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const pool = require('./db');
 
+
+const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Routes
 
@@ -11,9 +14,11 @@ app.get('/tasks', async (req, res) => {
     try{
         const allTasks = await pool.query('SELECT * FROM tasks');
         res.json(allTasks.rows);
+        console.log('Tasks fetched');
         console.log(allTasks.rows);
     }catch (err){
         console.error(err.message);
+        res.status(500).json({ error: err.message });
     }
 })
 
