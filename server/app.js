3591bb3beb60bf0d8fed018c55b139e6f49ccc8b
MrRozco <bryanorozco1997@hotmail.com>
2024-08-14 21:12:15 -0400

    whole app

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const pool = require('./db');
+const { v4: uuidv4 } = require('uuid');
 
 
 const app = express();
@@ -19,6 +20,18 @@ app.get('/tasks', async (req, res) => {
     }catch (err){
         console.error(err.message);
         res.status(500).json({ error: err.message });
+    }
+})
+
+//add a todo
+app.post('/tasks/:id', async (req, res) => {
+    const { title, progress, date} = req.body;
+    const id = uuidv4();
+    try {
+        const newTask = await pool.query('INSERT INTO tasks (id, title, progress) VALUES ($1, $2),', [id, title, progress]);
+        res.json(newTask);
+    } catch(error) {
+        console.error(error.message);
     }
 })
 
